88da2c856091455807a01058935b0a657e17f7d8
shanukumar59720 <shanukumar59720>
2024-02-21 12:23:39 +0530

    programming file is modified

--- programming.java
+++ programming.java
@@ -1,1 +1,7 @@
-programming is not that hard it's just that how much you practice
+//programming is not that hard it's just that how much you practice
+
+public class programming {
+    public static void main(String[] args) {
+        System.out.println("Hello java!");
+    }
+}
